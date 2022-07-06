170eecb74aabd52115e5e0faa178f68a4cfa7d81
robson3222 <robson3222@gmail.com>
2022-07-06 16:23:02 -0300

    listando imagens da api de filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import {useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link }from 'react-router-dom';
   
 // url da api: movie/now_playing?api_key=6a0aae5ada35be3e977a9b7c7871f9b6
 
@@ -30,10 +31,12 @@ setFilmes(response.data.results.slice(0,10))
     return(
         <div className='container'>
             <div className='"lista-filmes'>
-                {filmes.map((filme) =>{
+                {filmes.map((filme) => {
                     return(
                         <article key={filme.id}>
                             <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
                         </article>
                     )
                 })}
