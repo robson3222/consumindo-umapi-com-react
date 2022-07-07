00435c955b87fb7146fdcf7544512da94b332fd2
robson3222 <46981013+robson3222@users.noreply.github.com>
2022-07-07 11:03:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Erro(){
+    return(
+        <div className='not-found'>
+          <h1>404</h1>
+          <h2>Pagina não encontrada</h2>
+          <Link to="/">Veja todos os filmes</Link>
+        </div>
+    )
+}
+export default Erro;
